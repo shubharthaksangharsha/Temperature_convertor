5e0d1e148abc01d3f636c1a2fdb2d321790e52d2
shubharthaksangharsha <76090666+shubharthaksangharsha@users.noreply.github.com>
2021-01-24 21:01:43 +0530

    ex1.5.c
    // using for loop and printing in reverse order i.e from 300 to 0.

--- temp_conv_while.c
+++ temp_conv_while.c
@@ -1,19 +1,18 @@
+// using for loop and printing in reverse order i.e from 300 to 0.
 #include<stdio.h>
 void main()
 {
-   float cel, fah, upper, lower,change;
-   upper = 300, lower = 0, change=20;
-   fah = lower;
-   
-   
-   
-   while(fah<=upper)
-   {
-     cel = 5.0/9.0 * ( fah - 32 )  ;
+  float fah,cel;
+  for(fah = 300 ; fah >= 0 ; fah--)
+  {
+  cel = 5.0/9.0 * ( fah - 32 )  ;
      printf("Temperature in Fahernite: %3.1f \t \t" , fah);
      printf("Temperature in Celsius : %4.1f \n", cel);
-     fah =  fah + change;
+  
+  
+  }
    
-   }
+   
+  
 }
 
