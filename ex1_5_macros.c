28178800a7b4f7575a8fb0f1b7905fab0aa5cef1
shubharthaksangharsha <76090666+shubharthaksangharsha@users.noreply.github.com>
2021-01-24 21:10:48 +0530

    Create ex1_5_macros.c
    // using macros, for loop and printing in reverse order i.e from 300 to 0.

---
+++ ex1_5_macros.c
@@ -0,0 +1,20 @@
+// using macros, for loop and printing in reverse order i.e from 300 to 0.
+#include<stdio.h>
+#define LOWER 0;
+#define UPPER 300;
+#define CHANGE 20;
+void main()
+{
+  float fah,cel;
+  for(fah = UPPER; fah >= LOWER ; fah + CHANGE)
+  {
+  cel = 5.0/9.0 * ( fah - 32 )  ;
+     printf("Temperature in Fahernite: %3.1f \t \t" , fah);
+     printf("Temperature in Celsius : %4.1f \n", cel);
+  
+  
+  }
+   
+   
+  
+}
