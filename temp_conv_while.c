d89a95416dabc46365b53d9c7faa2242ae2f064c
shubharthaksangharsha <76090666+shubharthaksangharsha@users.noreply.github.com>
2021-01-24 18:01:43 +0530

    float_temp_conv.c
    Used float instead of integer for better result

--- temp_conv_while.c
+++ temp_conv_while.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-   int cel, fah, upper, lower,change;
+   float cel, fah, upper, lower,change;
    upper = 300, lower = 0, change=20;
    fah = lower;
    
@@ -9,9 +9,9 @@ void main()
    
    while(fah<=upper)
    {
-     cel = 5 * ( fah - 32 ) / 9 ;
-     printf("Temperature in Fahernite: %d \t \t" , fah);
-     printf("Temperature in Celsius : %d \n", cel);
+     cel = 5.0/9.0 * ( fah - 32 )  ;
+     printf("Temperature in Fahernite: %3.1f \t \t" , fah);
+     printf("Temperature in Celsius : %4.1f \n", cel);
      fah =  fah + change;
    
    }
